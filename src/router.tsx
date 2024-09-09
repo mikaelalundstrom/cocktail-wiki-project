f2432da455108be0c94fe93a205f1e7f6a27d4b5
mikaelalundstrom <mickismickis@hotmail.se>
2024-09-09 11:57:46 +0200

    Initial commit. Setup files

---
+++ src/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import App from "./App";
+import { LandingPage } from "./Pages/LandingPage";
+import { SearchPage } from "./Pages/SearchPage";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<LandingPage />} />
+      <Route path="/search" element={<SearchPage />} />
+    </Route>
+  )
+);
