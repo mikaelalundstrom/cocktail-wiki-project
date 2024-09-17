5699249998511f89f9fcaa3184a1285b14e1acd4
Nunzia Lampitelli <nunzia.lampitelli@outlook.com>
2024-09-17 22:47:05 +0200

    added not found page

--- src/router.tsx
+++ src/router.tsx
@@ -5,15 +5,18 @@ import SearchPage from "./Pages/SearchPage";
 import CocktailInfoPage from "./Pages/CocktailInfoPage";
 import FavoritesPage from "./Pages/FavoritesPage";
 import IngredientPage from "./Pages/IngredientPage";
+import NotFound from "./Pages/NotFoundPage";
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<LandingPage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/drink/:id" element={<CocktailInfoPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="/ingredient/:name" element={<IngredientPage />} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route index element={<LandingPage />} />
+			<Route path="/search" element={<SearchPage />} />
+			<Route path="/drink/:id" element={<CocktailInfoPage />} />
+			<Route path="/favorites" element={<FavoritesPage />} />
+			<Route path="/ingredient/:name" element={<IngredientPage />} />
+			<Route path="/not-found" element={<NotFound />} />
+			<Route path="*" element={<NotFound />} />
+		</Route>
+	)
 );
