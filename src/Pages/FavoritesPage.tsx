3e4d661d3dc9697d333cb20d927cd269f5eb827a
mikaelalundstrom <mickismickis@hotmail.se>
2024-09-17 09:59:01 +0200

    styling favorites page

--- src/Pages/FavoritesPage.tsx
+++ src/Pages/FavoritesPage.tsx
@@ -2,17 +2,19 @@ import { useContext } from "react";
 import { FavoritesContext } from "../context";
 import DrinkCard from "../Components/DrinkCard";
 
+import "./FavoritesPage.css";
+
 function FavoritesPage() {
   const { favoriteDrinks } = useContext(FavoritesContext);
   return (
-    <div>
-      <h1>Favorite cocktails</h1>
+    <>
+      <h1 className="favorites-title">Favorite cocktails</h1>
       <section className="favorites-grid">
         {favoriteDrinks?.map((drink) => (
           <DrinkCard key={drink.id} name={drink.name} id={drink.id} image={drink.image} />
         ))}
       </section>
-    </div>
+    </>
   );
 }
 
